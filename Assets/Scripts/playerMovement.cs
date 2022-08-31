0703a6c33fbe84aa3a19d8fbe33af187c75e4270
samulinukala <71631113+samulinukala@users.noreply.github.com>
2022-08-31 10:39:43 +0300

    initial progress
    movement and outer wall has been made working on projectiles

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private Vector3 playerLocation;
+    public float moveSpeed=13;
+
+    public float health=100;
+    public bool inTheLevel=false;
+    public Vector2 newPos = new Vector2();
+    public bool canFire;
+
+    public int score=0;
+   
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerLocation = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        checkCol();
+        
+    }
+    public void checkCol()
+    {
+        if (inTheLevel == false)
+        {
+
+            if (transform.position.x < -200)
+            {
+                newPos.x = -200;
+                newPos.y = transform.position.y;
+                transform.position = newPos;
+
+            }
+            else
+            if (transform.position.x > 200)
+            {
+                newPos.x = 200;
+                newPos.y = transform.position.y;
+                transform.position = newPos;
+            }
+            if (transform.position.y < -200)
+            {
+                newPos.y = -200;
+                newPos.x = transform.position.x;
+                transform.position = newPos;
+            }
+            else
+            if (transform.position.y > 200)
+            {
+                newPos.y = 200;
+                newPos.x = transform.position.x;
+                transform.position = newPos;
+            }
+
+        }
+
+    }
+    public void MovePlayer()
+    { 
+        // yksikkö vektori ottaa suunnan wasdista tällä hetkellä
+        Vector2 direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction.x = 1;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction.y = -1;
+        }
+        
+        Vector2 movement = new Vector2( direction.x*moveSpeed*Time.deltaTime ,  direction.y *moveSpeed * Time.deltaTime);
+        Debug.Log(movement);
+        transform.Translate(movement);
+        
+
+        // yksikkö vektori antaa suunnan joka kerrotaan movespeedillä ja delta timellä
+
+        //tämä lisätään sijaintiin
+    }
+    public void damagePlayerSlow(float _damage)
+    {
+        health=health - _damage*Time.deltaTime;
+        
+    }
+    
+   
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+            inTheLevel = false;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+            Debug.Log("colsty");
+            inTheLevel = true;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+            Debug.Log("colent");
+            inTheLevel = true;
+    }
+
+
+}
---
+++ Assets/Scripts/projectile.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+    public float moveSpeed = 50;
+    public Vector2 playerLoc;
+    public GameObject playerWhoShot;
+
+    public GameObject spawnPoint;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        playerWhoShot = GetComponentInParent<playerMovement>().gameObject;
+   
+        playerLoc = playerWhoShot.transform.position - transform.position;
+        transform.parent = null;
+
+        GetComponent<Rigidbody2D>().AddForce(-playerLoc.normalized * moveSpeed, ForceMode2D.Impulse);
+       
+    }
+
+    //Checks wall collision and destroys projectiles
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("wall")==true&&collision.gameObject.GetComponent<CharacterController>()==null)
+        {
+            Destroy(gameObject);
+        }
+       
+    }
+}
+
+
