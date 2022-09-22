717362c46ab0338dad42663a32a6c3337eb5850b
hamaoc <hamaoc@outlook.com>
2022-09-22 08:22:32 +0300

    added score

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -16,6 +16,7 @@ public class playerMovement : MonoBehaviour
     public int score=0;
     public Rigidbody2D Rigidbody2D;
     public float turnspeed = 5f;
+    public bool IsAlive => health < 0;
 
     // Start is called before the first frame update
     void Start()
---
+++ Assets/gameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public float survivalTimer;
+    public bool gameOver;
+    public TMP_Text scoreText;
+    public playerMovement playerMovement;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        playerMovement=FindObjectOfType<playerMovement>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        gameOver = playerMovement.IsAlive;
+        if (gameOver==false)
+        {
+            survivalTimer += 1 * Time.deltaTime;
+            scoreText.text =((int) survivalTimer).ToString();
+        }
+    }
+}
